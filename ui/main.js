a3f2dd3d90fcf622b1ed2100ff5f4a65158f9610
TejaswiChandan <31047181+TejaswiChandan@users.noreply.github.com>
2017-09-10 03:14:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,9 @@ var element = document.getElementById('main-text'
 );
 
 element.innerHTML = 'New Value';
+
+// Move the Image
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
