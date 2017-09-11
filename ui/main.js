0638e9017c858f12e253ee1afb6f83ce686f4aa7
TejaswiChandan <31047181+TejaswiChandan@users.noreply.github.com>
2017-09-12 01:02:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //Counter code
 var button = document.getElementById('counter');
 
-button.onClick = function () {
+button.onclick = function () {
     
     // Create a request object
     var request = new XMLHttpRequest();
